ddb8df445d450c1adfee4f4887cf9e71699788fa
emje-dania <emje7@student.eadania.dk>
2024-09-27 14:20:19 +0200

    saved for today 27-09

--- Game_Jeopardy_Emilie/Program.cs
+++ Game_Jeopardy_Emilie/Program.cs
@@ -8,58 +8,69 @@ namespace Game_Jeopardy_Emilie
 {
     internal class Program
     {
-        static int rightAnswer = 0;
-        static int wrongAnswer = 0;
-        
+        static string answer;
+
         static void Main(string[] args)
         {
             //varible list
             int points = 0;
-            int answer;
+            string answer;
+            string myanswer;
             int myscore = 0;
             
 
             //The intro, how to play and the score 
-            Console.WriteLine("Welcome to Jeopardy!\nHow To Play:\nPick a category by typing the corrosponding number\nof what you want to pick");
-            Console.WriteLine($"Your Score: {myscore}\n\n");
-            
-            //Ask a question and show 4 diffrent answers
-            //See if the choosen answer is correct
-            //If yes then gain points. If no then lose ponits
-            Console.WriteLine("What year is it?");
-            Console.WriteLine("1. 1995     2. 2000\n3. 2024     4. 2025");
-            answer = Convert.ToInt32(Console.ReadLine());
-            rightAnswer = 3;
+            Console.WriteLine("Welcome to Jeopardy!\nHow To Play:\nPick a category by typing the corrosponding number\nof what you want to pick.\nRemember to write: What is");
+            Console.WriteLine($"Your Score: {myscore}\n");
 
+            //Ask what category
+            //Ask how many points
+            //The player should get the question that corrosponds with the choosen points
+            Console.WriteLine("What Category do you want?");
+            Console.WriteLine("1. Dogs   2.Games   3. Cats   4.kfh   5.:)\n");
+            int choosenCategory = Convert.ToInt32(Console.ReadLine());
 
-            switch (answer)
-            {                
+            switch (choosenCategory)
+            {
                 case 1:
                     {
-                        WrongAnswer();
-                        points = -10;
+                        CategoryDogs();
+                        myanswer = Console.ReadLine();
+                        if (myanswer == DogBreedAnswer)
+                        {
+                            RightAnswer();
+                            myscore = +10;
+                        }
+                        else
+                        {
+                            WrongAnswer();
+                            myscore = -10;
+                        }
                         break;
                     }
                 case 2:
                     {
-                        WrongAnswer();
-                        points = -10;
-                        break;
-                    }
-                case 3:
-                    {
-                        RightAnswer();
-                        points = -10;
-                        break;
-                    }
-                case 4:
-                    {
-                        WrongAnswer();
-                        points = -10;
-                        break;
-                    }
 
+                        break;
+                    }
             }
+            Console.WriteLine("your score: "+myscore);
+
+            /*CategoryDogs();
+            myanswer = Console.ReadLine();
+
+            if (myanswer == DogBreedAnswer)
+            {
+                RightAnswer();
+            }
+            else
+            {
+                WrongAnswer();
+            }*/
+            
+            
+
+            
                         
             Console.ReadLine();
         }
@@ -68,7 +79,7 @@ namespace Game_Jeopardy_Emilie
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Wrong Answer");
             Console.ResetColor();
-            Console.WriteLine($"The anwser was {rightAnswer}");
+            
         }
 
         static void RightAnswer()
@@ -76,6 +87,22 @@ namespace Game_Jeopardy_Emilie
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Correct!");
             Console.ResetColor();
+        }
+
+        /// <summary>
+        /// The questions for the Dog category
+        /// </summary>
+        static void CategoryDogs()
+        {
+            Console.WriteLine("The biggest dog breed");
+            
+        }
+        //the answer for biggest dog breed
+        static string DogBreedAnswer = "what is a mastiff";
+
+        static void CategoryGame()
+        {
+            Console.WriteLine("");
         }
     }
 }
