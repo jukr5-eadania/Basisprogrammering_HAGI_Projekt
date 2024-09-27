ad0217c00d9f61cfa7d56b54af3610c4c81a3c0a
emje-dania <emje7@student.eadania.dk>
2024-09-27 12:47:01 +0200

    Wrong and Right answer functions work
    yay

--- Game_Jeopardy_Emilie/Program.cs
+++ Game_Jeopardy_Emilie/Program.cs
@@ -8,8 +8,74 @@ namespace Game_Jeopardy_Emilie
 {
     internal class Program
     {
+        static int rightAnswer = 0;
+        static int wrongAnswer = 0;
+        
         static void Main(string[] args)
         {
+            //varible list
+            int points = 0;
+            int answer;
+            int myscore = 0;
+            
+
+            //The intro, how to play and the score 
+            Console.WriteLine("Welcome to Jeopardy!\nHow To Play:\nPick a category by typing the corrosponding number\nof what you want to pick");
+            Console.WriteLine($"Your Score: {myscore}\n\n");
+            
+            //Ask a question and show 4 diffrent answers
+            //See if the choosen answer is correct
+            //If yes then gain points. If no then lose ponits
+            Console.WriteLine("What year is it?");
+            Console.WriteLine("1. 1995     2. 2000\n3. 2024     4. 2025");
+            answer = Convert.ToInt32(Console.ReadLine());
+            rightAnswer = 3;
+
+
+            switch (answer)
+            {                
+                case 1:
+                    {
+                        WrongAnswer();
+                        points = -10;
+                        break;
+                    }
+                case 2:
+                    {
+                        WrongAnswer();
+                        points = -10;
+                        break;
+                    }
+                case 3:
+                    {
+                        RightAnswer();
+                        points = -10;
+                        break;
+                    }
+                case 4:
+                    {
+                        WrongAnswer();
+                        points = -10;
+                        break;
+                    }
+
+            }
+                        
+            Console.ReadLine();
+        }
+        static void WrongAnswer()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Wrong Answer");
+            Console.ResetColor();
+            Console.WriteLine($"The anwser was {rightAnswer}");
+        }
+
+        static void RightAnswer()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Correct!");
+            Console.ResetColor();
         }
     }
 }
