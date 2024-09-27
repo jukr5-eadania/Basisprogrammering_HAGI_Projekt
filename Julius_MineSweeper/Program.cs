c224a1931f469adb57267f19a36616245a57ff78
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-27 14:32:24 +0200

    new board gets created

--- Julius_MineSweeper/Program.cs
+++ Julius_MineSweeper/Program.cs
@@ -14,6 +14,9 @@ namespace Julius_MineSweeper
         Three,
         Four,
         Five,
+        Six,
+        Seven,
+        Eight,
         Bomb
 
     }
@@ -21,8 +24,8 @@ namespace Julius_MineSweeper
     internal class Program
     {
         static Random rnd = new Random();
-        static int[,] newMSB = new int[8, 8];
-        static int[,] playerMSB = new int[8, 8];
+        static int[,] newMSB = new int[10, 10];
+        static int[,] playerMSB = new int[10, 10];
         static int[] sideGrid = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
         static int sideGridNumber = 0;
 
@@ -59,20 +62,67 @@ namespace Julius_MineSweeper
         {
             sideGridNumber = 0;
             Console.WriteLine("    a   b   c   d   e   f   h   i");
-            
+
             for (int a = 0; a < 11; a++)
             {
-                int rndX = rnd.Next(0, 8);
-                int rndY = rnd.Next(0, 8);
-                newMSB[rndX, rndY] = (int) Spaces.Bomb;
+                int rndX = rnd.Next(1, 9);
+                int rndY = rnd.Next(1, 9);
+                newMSB[rndX, rndY] = (int)Spaces.Bomb;
             }
 
-            for (int x = 0; x < newMSB.GetLength(0); x++)
+            for (int x = 1; x < newMSB.GetLength(0) - 1; x++)
+            {
+                for (int y = 1; y < newMSB.GetLength(1) - 1; y++)
+                {
+                    if (newMSB[x, y] != (int)Spaces.Bomb)
+                    {
+                        int bombs = 0;
+                        //checker de 8 nærlæggende felter for bomber og lægger det passende tal til på feltet
+                        if (newMSB[x - 1, y - 1] == (int)Spaces.Bomb)
+                        {
+                            bombs++;
+                        }
+                        if (newMSB[x, y - 1] == (int)Spaces.Bomb)
+                        {
+                            bombs++;
+                        }
+                        if (newMSB[x + 1, y - 1] == (int)Spaces.Bomb)
+                        {
+                            bombs++;
+                        }
+                        if (newMSB[x - 1, y] == (int)Spaces.Bomb)
+                        {
+                            bombs++;
+                        }
+                        if (newMSB[x + 1, y] == (int)Spaces.Bomb)
+                        {
+                            bombs++;
+                        }
+                        if (newMSB[x - 1, y + 1] == (int)Spaces.Bomb)
+                        {
+                            bombs++;
+                        }
+                        if (newMSB[x, y + 1] == (int)Spaces.Bomb)
+                        {
+                            bombs++;
+                        }
+                        if (newMSB[x + 1, y + 1] == (int)Spaces.Bomb)
+                        {
+                            bombs++;
+                        }
+                        newMSB[x, y] = (int) ((Spaces)bombs);
+                    }
+
+                }
+
+            }
+
+            for (int x = 1; x < newMSB.GetLength(0) - 1; x++)
             {
                 Console.WriteLine("  * - * - * - * - * - * - * - * - *");
                 Console.Write(sideGrid[sideGridNumber] + " | ");
                 sideGridNumber++;
-                for (int y = 0; y < newMSB.GetLength(1); y++)
+                for (int y = 1; y < newMSB.GetLength(1) - 1; y++)
                 {
                     Console.Write(newMSB[x, y] + " ");
                     Console.Write("| ");
